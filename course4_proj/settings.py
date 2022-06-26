e01660daac5f2c4d3346e3657bbbaad98c93ecc1
Muhab Seif <mohab.seif.16@gmail.com>
2022-06-26 19:03:39 +0000

    Finish celery introduction

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -43,6 +43,7 @@ class Dev(Configuration):
         'django.contrib.staticfiles',
         'movies',
         'gh',
+        'django_celery_results',
         ]
 
     MIDDLEWARE = [
@@ -161,3 +162,6 @@ class Dev(Configuration):
     SESSION_COOKIE_SAMESITE = "None"    
 
     OMDB_KEY = "5d9e8779"
+
+    CELERY_RESULT_BACKEND = "django-db"
+    CELERY_BROKER_URL = "redis://localhost:6379/0"
